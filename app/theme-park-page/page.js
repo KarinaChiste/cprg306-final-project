21ebed80feb032ec41af398eb819c36c9e240965
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-12-15 22:21:46 -0700

    Theme Park Page Updated

--- app/theme-park-page/page.js
+++ app/theme-park-page/page.js
@@ -17,7 +17,10 @@ export default function Page(){
   return(
     <main className="min-h-screen bg-cover bg-center bg-fixed"
       style={{ backgroundImage: "url('/castle.jpg')" }}>
-      <h1 className="text-white text-4xl text-center font-[Luckiest Guy] ">Once Upon a Character</h1>
+        <div className="flex justify-center my-2">
+          <img src="https://fontmeme.com/permalink/251215/e60a35015e19295ffc251891d2b33320.png" alt="Once upon a Character" border="0" />
+        </div>
+      {/* <h1 className="text-white text-4xl text-center font-[Luckiest Guy] ">Once Upon a Character</h1> */}
       <Link href={"../"} className="bg-green-300 p-5 border-black m-20 rounded-4xl my-50">Main Page</Link>
       <div className="justify-center flex">
         <Search onSearch={setCharName}/>
